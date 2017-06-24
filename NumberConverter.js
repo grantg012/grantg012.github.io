5ad275efb4f9c289a1639830142620125b1a2106
grantg012 <grantg012@users.noreply.github.com>
2017-06-24 12:42:24 -0400

    Delete NumberConverter.js

--- NumberConverter.js
+++
@@ -1,9 +0,0 @@
-function FieldThree() {
-    $("#ResultThree").text(Converter2($("#NumThree").val(),$("#InpType").val(),$("#OutType").val()));
-}
-
-function FieldThreeText(e) {
-    if(e.keyCode == 13){
-        FieldThree()
-    }
-}
